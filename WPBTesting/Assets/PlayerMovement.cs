9cdebd5d4ee9b1cd1810e8608363c0188ce4fc98
Benjamin James Hallsted <bhallste@crm.ucsc.edu>
2018-01-12 16:21:16 -0800

    added basic firing thing needs lots of work but is there

--- WPBTesting/Assets/PlayerMovement.cs
+++ WPBTesting/Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
@@ -11,5 +13,26 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // not destroying bullet yet, letting it go free
+        // Destroy the bullet after 2 seconds
+        // Destroy(bullet, 2.0f);
     }
 }
