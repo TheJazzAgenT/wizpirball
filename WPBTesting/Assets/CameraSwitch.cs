64e3db6dbaadf5c85fe5607ec66706de04263686
Benjamin Hallsted <bhallste@ucsc.edu>
2018-01-26 14:57:46 -0800

    camera switching working collison still not working as intended.

---
+++ WPBTesting/Assets/CameraSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+    public Camera ShipCamera;
+    public Camera PlayerCamera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (ShipCamera.enabled == true)
+            {
+                ShowPlayerView();
+            }
+            else
+            {
+                ShowShipView();
+            }
+        }
+    }
+
+    public void ShowPlayerView()
+    {
+        ShipCamera.enabled = false;
+        PlayerCamera.enabled = true;
+    }
+
+    public void ShowShipView()
+    {
+        ShipCamera.enabled = true;
+        PlayerCamera.enabled = false;
+    }
+
+
+}
