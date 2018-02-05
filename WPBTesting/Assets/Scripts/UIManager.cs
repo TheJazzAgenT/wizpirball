84ff3cd00dfacda970542dd70cb622546111c5b3
Benjamin James Hallsted <bhallste@crm.ucsc.edu>
2018-02-05 12:41:12 -0800

    made pause menu, updatyed main menu a bit

---
+++ WPBTesting/Assets/Scripts/UIManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        HidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                ShowPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                HidePaused();
+            }
+        }
+    }
+    //to load or restart a scene use LoadSceneOnClick instead
+
+    //controls the pausing of the scene
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            ShowPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            HidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void ShowPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void HidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+}
