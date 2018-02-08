887506a5f2747824c825f5a87d90e48f266f119f
thejazzagent <ethomson@ucsc.edu>
2018-02-08 13:51:47 -0800

    added new player in, with a start on animations

--- WPBTesting/Assets/Scripts/CharacterMovement.cs
+++ WPBTesting/Assets/Scripts/CharacterMovement.cs
@@ -34,43 +34,16 @@ public class CharacterMovement : MonoBehaviour {
     void FixedUpdate () {
         if (PlayerCamera.enabled)
         {
-            // Generate a plane that intersects the transform's position with an upwards normal.
-            /*Plane playerPlane = new Plane(Vector3.up, transform.position);
-
-            // Generate a ray from the cursor position
-            Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
-
-            // Determine the point where the cursor ray intersects the plane.
-            // This will be the point that the object must look towards to be looking at the mouse.
-            // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
-            //   then find the point along that ray that meets that distance.  This will be the point
-            //   to look at.
-            float hitdist = 0.0f;
-            // If the ray is parallel to the plane, Raycast will return false.
-            if (playerPlane.Raycast(ray, out hitdist))
-            {
-                // Get the point along the ray that hits the calculated distance.
-                Vector3 targetPoint = ray.GetPoint(hitdist);
-
-                // Determine the target rotation.  This is the rotation if the transform looks at the target point.
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-
-                // Smoothly rotate towards the target point.
-                //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-                transform.rotation = targetRotation;
-
-            }*/
-
             verticalInput = Input.GetAxis("Vertical");
             horizontalInput = Input.GetAxis("Horizontal");
             transform.Translate(horizontalInput * speed, 0.0f, verticalInput * speed);
             if (verticalInput != 0 || horizontalInput !=0)
             {
-                anim.SetTrigger("isRunning");
+                anim.SetTrigger("isMoving");
             }
             else
             {
-                anim.SetTrigger("StoppedRunning");
+                //anim.SetTrigger("");
             }
             
         }
@@ -91,6 +64,7 @@ public class CharacterMovement : MonoBehaviour {
     void Fire()
     {
         // Create the Bullet from the Bullet Prefab
+        anim.SetTrigger("isHitting");
         var bullet = (GameObject)Instantiate(
             bulletPrefab,
             bulletSpawn.position,
