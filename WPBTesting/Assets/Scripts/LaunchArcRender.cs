1f834ced25af3bebab62896613e57882a3107a89
Benjamin Hallsted <bhallste@ucsc.edu>
2018-03-04 17:12:37 -0800

    startinf stuff for projectile arc

---
+++ WPBTesting/Assets/Scripts/LaunchArcRender.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class LaunchArcRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
