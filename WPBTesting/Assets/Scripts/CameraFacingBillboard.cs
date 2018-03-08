027bd1d72f2cebb971c69278690d622631966ccd
Michael Eugene Harrold <mharrold@crm.ucsc.edu>
2018-03-07 16:34:01 -0800

    adjusted particles, improved aiming reticule

---
+++ WPBTesting/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera Cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Cam.transform.position, Vector3.up);
+    }
+}
