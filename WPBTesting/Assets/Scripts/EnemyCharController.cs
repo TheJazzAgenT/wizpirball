1ea62511f13a71914f8309f45fa5baf5e6033a43
Evan Thomson <ethomson@ucsc.edu>
2018-03-08 12:54:53 -0800

    now an enemy dude hittin balls in your face

--- WPBTesting/Assets/Scripts/EnemyCharController.cs
+++ WPBTesting/Assets/Scripts/EnemyCharController.cs
@@ -8,17 +8,16 @@ public class EnemyCharController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float BulletSpeed = 1.0f;
-    public GameObject spawnPoint;
     public Transform farEnd;
     public Transform nearEnd;
     public GameObject playerShip;
-    private float secondsForOneLength = 10f;
-    bool canRespawn = true;
+    private float secondsForOneLength = 5f;
     float timer = 0.0f;
     float shootDelay = 5.0f;
 
     void Start()
     {
+        transform.position = nearEnd.position;
     }
 
     void Update()
@@ -33,20 +32,6 @@ public class EnemyCharController : MonoBehaviour
         // AI Paces between nearEnd and farEnd
         transform.position = Vector3.Lerp(nearEnd.position, farEnd.position, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
         
-    }
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.transform.tag == "WATER" && canRespawn)
-        {
-            canRespawn = false;
-            Invoke("respawnPlayer", 5);
-        }
-    }
-    void respawnPlayer()
-    {
-        this.transform.position = spawnPoint.transform.position;
-        this.transform.rotation = spawnPoint.transform.rotation;
-        canRespawn = true;
     }
 
     void Fire()
