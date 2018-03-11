f3754d02e345b4c118646abb6a93b1f2e85ccc14
meharrold <meharrold@comcast.net>
2018-03-10 16:09:24 -0800

    partial fix to the animation problem

---
+++ WPBTesting/Assets/Scripts/LegAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegAnimation : MonoBehaviour {
+
+	public Animator LegAnimator;
+	// Use this for initialization
+	void Start () {
+		LegAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("w")) {
+			LegAnimator.Play ("Armature|FeetMove");
+		}
+		if (Input.GetKeyUp ("w")) {
+			LegAnimator.Play ("Armature|Empty");
+		}
+
+	}
+}
