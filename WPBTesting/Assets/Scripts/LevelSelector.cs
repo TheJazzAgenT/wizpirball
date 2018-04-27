4ab267b2ee2f484797ead4a57dc3bcaa445ad515
Benjamin Hallsted <bhallste@ucsc.edu>
2018-04-27 01:01:10 -0700

    created a level selector for the 1v1s, just need to make usre the maps we use are set for 1v1s, using the canyons fo doom as a placeholder for now

---
+++ WPBTesting/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour {
+
+    GameObject MapA;
+    GameObject MapB;
+    GameObject description;
+
+    Text descript;
+	// Use this for initialization
+	void Start () {
+        MapA = GameObject.Find("MapAPic");
+        MapB = GameObject.Find("MapBPic");
+        description = GameObject.Find("Map_Descript");
+
+        descript = description.GetComponent<Text>();
+
+        descript.text = " ";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonHover(Button button)
+    {
+        if (button.name == "Map1Button")//horseshoe island thing
+        {
+            MapA.transform.SetAsLastSibling();
+            descript.text = " island shaped like a horseshoe, the edge of the Empires control";
+        }
+        else if (button.name == "Map2Button")//canyons of doom
+        {
+            MapB.transform.SetAsLastSibling();
+            descript.text = " Canyons that many have tried to navigate and failed, then again they never thought to use magic";
+        }
+    }
+}
