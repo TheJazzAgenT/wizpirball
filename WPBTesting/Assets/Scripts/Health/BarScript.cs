ee547f6a1bc6d51856511def70015438dc7fe518
Evan Thomson <ethomson@ucsc.edu>
2018-05-09 18:18:24 -0700

    fixed bug with bar animations

--- WPBTesting/Assets/Scripts/Health/BarScript.cs
+++ WPBTesting/Assets/Scripts/Health/BarScript.cs
@@ -12,9 +12,11 @@ public class BarScript : MonoBehaviour {
 	private Image content;
     private float maxHealth;
     private float prevfillAmount;
-    private float lerpTime = 2.0f;
+    private float lerpTime = 1.0f;
     private float timer = 0.0f;
     private IEnumerator coroutine;
+	private Queue damageQ = new Queue();
+	private Vector2 curDamage;
 
 
     public Text Health;
@@ -25,6 +27,7 @@ public class BarScript : MonoBehaviour {
         prevfillAmount = fillAmount;
 		//fillAmount = 100;//this is causing some problems
 		Health.text = fillAmount.ToString ();
+		StartCoroutine (LerpBar());
 	}
 	
 	// Update is called once per frame
@@ -38,28 +41,48 @@ public class BarScript : MonoBehaviour {
 		//Health.text = fillAmount.ToString ();
         if (fillAmount != prevfillAmount)
         {
-            if (prevfillAmount - fillAmount < 2)
-            {
-                content.fillAmount = Map(fillAmount, 0, maxHealth, 0, 1);
-                Health.text = fillAmount.ToString ();
-                prevfillAmount = fillAmount;
-                return;
-            }
-            StartCoroutine(LerpBar(prevfillAmount, fillAmount));
-            prevfillAmount = fillAmount;
+			/*if (timer == 0) {
+				if (prevfillAmount - fillAmount < 2) {
+					content.fillAmount = Map (fillAmount, 0, maxHealth, 0, 1);
+					Health.text = fillAmount.ToString ();
+					prevfillAmount = fillAmount;
+					return;
+				}
+				StartCoroutine (LerpBar (prevfillAmount, fillAmount, 0));
+				prevfillAmount = fillAmount;
+			} 
+			else {
+				if (prevfillAmount - fillAmount < 2) {
+					content.fillAmount = Map (fillAmount, 0, maxHealth, 0, 1);
+					Health.text = fillAmount.ToString ();
+					prevfillAmount = fillAmount;
+					return;
+				}
+				StartCoroutine (LerpBar (prevfillAmount, fillAmount, lerpTime - timer));
+				prevfillAmount = fillAmount;
+			}*/
+			damageQ.Enqueue (new Vector2(prevfillAmount, fillAmount));
+			prevfillAmount = fillAmount;
         }
 	}
 
-    private IEnumerator LerpBar(float prev, float cur)
+	private IEnumerator LerpBar()
     {
-        while (timer < lerpTime)
-        {
-            content.fillAmount = Map(Mathf.Lerp(prev, cur, timer), 0, maxHealth, 0, 1);
-            timer += Time.deltaTime;
-            Health.text = fillAmount.ToString();
-            yield return null;
-        }
-        timer = 0;
+		//yield return new WaitForSeconds(delay);
+		while (true) {
+			if (damageQ.Count > 0) {
+				curDamage = (Vector2)damageQ.Dequeue ();
+				while (timer < lerpTime) {
+					content.fillAmount = Map (Mathf.Lerp (curDamage[0], curDamage[1], timer / lerpTime), 0, maxHealth, 0, 1);
+					timer += Time.deltaTime;
+					Health.text = fillAmount.ToString ();
+					yield return null;
+				}
+				timer = 0;
+			} else {
+				yield return null;
+			}
+		}
     }
 
 	private float Map(float value, float inMin, float inMax, float outMin, float outMax)
