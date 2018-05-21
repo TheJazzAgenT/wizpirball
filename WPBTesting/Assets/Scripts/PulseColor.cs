637e8c56beead4ba222761c9b2dd13dcea34df06
Evan Thomson <ethomson@ucsc.edu>
2018-05-21 16:41:01 -0700

    images now pulse their color

---
+++ WPBTesting/Assets/Scripts/PulseColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PulseColor : MonoBehaviour {
+    private RawImage img;
+    private Color m_Color;
+	// Use this for initialization
+	void Start () {
+        img = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 1));
+        img.color = m_Color;
+    }
+}
