d71b5a2f7fadf58eeae7cf699f737e7115743252
Evan Thomson <ethomson@ucsc.edu>
2018-05-24 15:20:29 -0700

    targeter no longer gets moved by ship rotating

---
+++ WPBTesting/Assets/IgnoreRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreRotation : MonoBehaviour {
+
+    public GameObject ship; //assign the camera in inspector or in Start()
+    private Vector3 prevPos;
+
+    void Start()
+    {
+        prevPos = ship.transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 displacement = ship.transform.position - prevPos;
+
+        prevPos = ship.transform.position;
+
+        this.transform.position += displacement;
+    }
+}
