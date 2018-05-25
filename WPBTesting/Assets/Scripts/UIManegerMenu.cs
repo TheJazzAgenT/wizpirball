a296efd6b1593a09e15fdd840c3e69dd61fc932c
Benjamin Hallsted <bhallste@ucsc.edu>
2018-05-24 21:20:33 -0700

    redid everythign I did, I think.

---
+++ WPBTesting/Assets/Scripts/UIManegerMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManegerMenu : MonoBehaviour
+{
+    //this exists just to make sure time scale is on when heading back to menu
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //to load or restart a scene use LoadSceneOnClick instead
+}
