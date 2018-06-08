adaf1189829e270dd2741720fb291fbd40efd642
Evan Thomson <ethomson@ucsc.edu>
2018-06-08 16:21:45 -0700

    counter hit hits enemy ball back instead of making new one

--- WPBTesting/Assets/Scripts/CounterHit.cs
+++ WPBTesting/Assets/Scripts/CounterHit.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CounterHit : MonoBehaviour {
-    public GameObject bat; // To be used for animation purposes.
+    //public GameObject bat; // To be used for animation purposes.
     public Transform batSpawn; // Where to spawn the bat, probably Hand.R
+    public Transform enemyShip;
+    public Transform target;
 
     [SerializeField]
     private GameObject myCharacter; // Whichever character should be doing the counter hitting
@@ -41,10 +43,14 @@ public class CounterHit : MonoBehaviour {
             //Debug.Log("baseball entered");
             if ((Input.GetKeyDown(KeyCode.Mouse1) || (Input.GetAxis(controller.playerInput[10]) > 0)) && timer > counterDelay)
             {
+                Debug.Log("CounterHit!-----");
+                Vector3 vel = other.GetComponent<Rigidbody>().velocity;
+                //other.GetComponent<Rigidbody>().velocity = (-other.GetComponent<Rigidbody>().velocity + 1.5f * (enemyShip.position - other.transform.position));
+                other.GetComponent<Rigidbody>().velocity = 1 * vel.magnitude * ((target.position - other.transform.position).normalized + new Vector3(0, 0.5f, 0));
                 //throwBat = (GameObject)Instantiate(bat, batSpawn.position, batSpawn.rotation);
-                controller.Fire();
+                //controller.Fire();
                 controller.mana -= 10;
-                Destroy(other.gameObject);
+                //Destroy(other.gameObject);
                 timer = 0;
             }
         }
