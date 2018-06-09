11978e225f168b34b02fcc02bbc8c9216fea8399
Benjamin Hallsted <bhallste@ucsc.edu>
2018-06-08 19:20:01 -0700

    partly fixed highlighting thing

--- WPBTesting/Assets/Scripts/SelectOnStart.cs
+++ WPBTesting/Assets/Scripts/SelectOnStart.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SelectOnInput : MonoBehaviour {
+public class SelectOnStart : MonoBehaviour {
 
     public EventSystem eventSystem;
     public GameObject selectedObject;
 
-    private bool buttonSelected;
+    private bool buttonSelected = false;
 
     // Use this for initialization
     void Start()
@@ -23,11 +23,7 @@ public class SelectOnInput : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
-        {
-            eventSystem.SetSelectedGameObject(selectedObject);
-            buttonSelected = true;
-        }
+
     }
 
     private void OnDisable()
